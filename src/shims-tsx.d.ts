e939c0a3bbd589f5f7d84506734551d0e07ed1b9
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-05-18 21:27:46 +0900

    Use ComponentOptions

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,13 +1,16 @@
-// import Vue, { VNode } from "vue";
+import Vue, { VNode, ComponentRenderProxy } from 'vue'
 
-// declare global {
-//   namespace JSX {
-//     // tslint:disable no-empty-interface
-//     interface Element extends VNode {}
-//     // tslint:disable no-empty-interface
-//     interface ElementClass extends Vue {}
-//     interface IntrinsicElements {
-//       [elem: string]: any;
-//     }
-//   }
-// }
+declare global {
+    namespace JSX {
+        // tslint:disable no-empty-interface
+        interface Element extends VNode {}
+        // tslint:disable no-empty-interface
+        interface ElementClass extends ComponentRenderProxy {}
+        interface ElementAttributesProperty {
+            $props: any;
+        }
+        interface IntrinsicElements {
+            [elem: string]: any;
+        }
+    }
+}
