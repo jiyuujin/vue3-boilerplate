bcad3393a20d462ca6eddd278e81fcb9874d6ea4
yuma kitamura <yuma.kitamura@smaregi.jp>
2020-07-01 15:17:05 +0900

    next update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import App from "./App.vue";
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-createApp(App).mount('#app');
+createApp(App).mount("#app");
--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,16 +1,16 @@
-import Vue, { VNode, ComponentRenderProxy } from 'vue'
+import Vue, { VNode, ComponentRenderProxy } from "vue";
 
 declare global {
-    namespace JSX {
-        // tslint:disable no-empty-interface
-        interface Element extends VNode {}
-        // tslint:disable no-empty-interface
-        interface ElementClass extends ComponentRenderProxy {}
-        interface ElementAttributesProperty {
-            $props: any;
-        }
-        interface IntrinsicElements {
-            [elem: string]: any;
-        }
+  namespace JSX {
+    // tslint:disable no-empty-interface
+    interface Element extends VNode {}
+    // tslint:disable no-empty-interface
+    interface ElementClass extends ComponentRenderProxy {}
+    interface ElementAttributesProperty {
+      $props: any;
     }
+    interface IntrinsicElements {
+      [elem: string]: any;
+    }
+  }
 }
